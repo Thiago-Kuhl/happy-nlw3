cce793c378103ba6c859ee8a337fbf57900ef242
thiago-kuhl <thiago.kuhl15@gmail.com>
2020-10-14 23:23:32 -0300

    Creating landing page and orphanagesMap page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
+import './styles/global.css'
 
+import Routes from "./routes"
 //JSX - JavaScript XML
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+  <Routes />
   );
 }
 
